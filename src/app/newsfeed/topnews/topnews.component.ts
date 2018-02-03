5fb9e2a06e3bfb458762721779fa61323b6b7c9b
Eugene <inhuman.en@gmail.com>
2018-02-03 20:41:15 +0200

    style: list appearance animation

--- src/app/newsfeed/topnews/topnews.component.ts
+++ src/app/newsfeed/topnews/topnews.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import {
+    trigger,
+    state,
+    style,
+    animate,
+    transition,
+    keyframes
+} from '@angular/animations';
 import { HackerNewsService } from '../../shared/index';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
@@ -9,13 +17,30 @@ import 'rxjs/add/operator/share';
 @Component({
     selector: 'hn-topnews',
     templateUrl: './topnews.component.html',
-    styleUrls: ['./topnews.component.scss']
+    styleUrls: ['./topnews.component.scss'],
+    animations: [
+        trigger('appear', [
+            state('hidden', style({ opacity: '.1' })),
+            state('visible', style({ opacity: '1'  })),
+            transition('hidden => visible', [
+                animate(
+                    400,
+                    keyframes([
+                        style({ opacity: '.1' , offset: 0 }),
+                        style({ opacity: '1' , offset: 1.0 })
+                    ])
+                )
+            ])
+        ])
+    ]
 })
 export class TopnewsComponent implements OnInit {
     currentPage = 1;
     showSpinner = false;
     topNews$: Observable<any>;
     page$: Observable<number>;
+
+    newsDisplayState: string;
 
     constructor(
         private service: HackerNewsService,
@@ -34,9 +59,14 @@ export class TopnewsComponent implements OnInit {
                 () => this.toggleSpinner(false),
                 () => this.toggleSpinner(false)
             )
+            .do(() => this.setAppearanceState('visible'))
             .share();
 
         this.showSpinner = true;
+    }
+
+    private setAppearanceState(newState: string) {
+        this.newsDisplayState = newState;
     }
 
     private toggleSpinner(on: boolean) {
@@ -55,6 +85,7 @@ export class TopnewsComponent implements OnInit {
         this.page$.subscribe((page: number) => {
             this.fetchNewsByPage(page);
             this.updateCurrentPage(page);
+            this.setAppearanceState('hidden');
         });
     }
 }
